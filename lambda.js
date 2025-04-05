302017d397f4dbaede9867a79875cd89fa9974c5
Ashish <terminal777888@gmail.com>
2025-04-05 19:13:07 +0530

    enabling log

--- lambda.js
+++ lambda.js
@@ -1,4 +1,4 @@
 const serverlessExpress = require('@vendia/serverless-express');
 const app = require('./server'); // Your Express app
 
-exports.handler = serverlessExpress({ app });
+exports.handler = serverlessExpress({ app, log: true,  });
--- server.js
+++ server.js
@@ -4,19 +4,13 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  const {query_input} = req.query
-  res.json({ message: `Hello from Lambda! root end point ${query_input}` });
+  const { query_input } = req.query;
+  res.json({ message: `Hello from Lambda! root endpoint ${query_input}` });
 });
 
 app.get("/home-page", (req, res) => {
-  const {query_input} = req.query
+  const { query_input } = req.query;
   res.json({ message: `Hello from Lambda Home page! ${query_input}` });
 });
-
-// Run server only when executed directly (not in Lambda)
-if (require.main === module) {
-  const PORT = process.env.PORT || 4000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
 
 module.exports = app;
