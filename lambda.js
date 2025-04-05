cf7628906992bde445125989f802e389aed8464f
Ashish <terminal777888@gmail.com>
2025-04-05 20:20:13 +0530

    11

--- lambda.js
+++ lambda.js
@@ -1,9 +1,4 @@
 const serverlessExpress = require('@vendia/serverless-express');
 const app = require('./server'); // Your Express app
 
-exports.handler = async (event, context) => {
-    console.log("Event:", JSON.stringify(event));
-    console.log("Context:", JSON.stringify(context));
-    const handler = serverlessExpress({ app });
-    return handler(event, context);
-  };
+exports.handler = serverlessExpress({ app });
