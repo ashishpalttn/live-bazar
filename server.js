97e6bb54e31b37265ab72ea147d37a6a7c0555b2
Ashish <terminal777888@gmail.com>
2025-04-05 11:09:37 +0530

    test modification 2

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello from Lambda modified_1!" });
 });
 // Run server only when executed directly (not in Lambda)
+// Run server only when executed directly (not in Lambda)
 if (require.main === module) {
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
