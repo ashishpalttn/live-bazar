6c0df9d5e037b8637d7b3314c16c1ac74c22e845
Ashish <terminal777888@gmail.com>
2025-04-05 19:28:04 +0530

    update 1

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const { query_input } = req.query;
-  res.json({ message: `Hello from Lambda! root endpoint ${query_input}` });
+  res.json({ message: `Hello from Lambda! root end point ${query_input}` });
 });
 
-app.get("/home-page", (req, res) => {
+app.get('/home-page', (req, res) => {
   const { query_input } = req.query;
   res.json({ message: `Hello from Lambda Home page! ${query_input}` });
 });
