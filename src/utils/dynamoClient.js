1075acab145dde66a3062d7b668928192b190335
Ashish <terminal777888@gmail.com>
2025-04-16 00:02:59 +0530

    updating dynamo client

--- src/utils/dynamoClient.js
+++ src/utils/dynamoClient.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
 
 // Debug log to verify the AWS object
-console.log("AWS SDK Version:", AWS.VERSION);
+console.log("AWS SDK Version:", AWS.VERSION || "Built-in AWS SDK not found");
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
