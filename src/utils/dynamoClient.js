72a520d1f104b6c84ed4e48e8dfd53a7ad10aca8
Ashish <terminal777888@gmail.com>
2025-04-15 23:44:46 +0530

    updating dynamo client

--- src/utils/dynamoClient.js
+++ src/utils/dynamoClient.js
@@ -1,5 +1,8 @@
 const AWS = require('aws-sdk');
 
+// Debug log to verify the AWS object
+console.log("AWS SDK Version:", AWS.VERSION);
+
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports = dynamoDb;
