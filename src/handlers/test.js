3435580a278cc021a8e2dd97c777d40125cb746d
Ashish <terminal777888@gmail.com>
2025-04-18 22:54:58 +0530

    add error handling to test function in handler

--- src/handlers/test.js
+++ src/handlers/test.js
@@ -1,6 +1,11 @@
 const AWS = require('aws-sdk');
 
 exports.handler = async () => {
-    console.log("AWS SDK Version:", AWS.VERSION || "Built-in AWS SDK not found");
-    return { statusCode: 200, body: JSON.stringify({ message: "Test function executed successfully" }) };
+    try {
+        console.log("AWS SDK Version:", AWS.VERSION || "Built-in AWS SDK not found");
+        return { statusCode: 200, body: JSON.stringify({ message: "Test function executed successfully" }) };
+    } catch (error) {
+        console.error("Error in testFunction:", error);
+        return { statusCode: 500, body: JSON.stringify({ message: "Internal server error", error: error.message }) };
+    }
 };
